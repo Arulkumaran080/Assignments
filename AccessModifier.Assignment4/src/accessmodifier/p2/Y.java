97a27a91565be46d1f142046820efb2c822da9ec
Arulkumaran080 <kumaranarul080@gmail.com>
2022-07-26 13:11:43 +0530

    Practice

---
+++ AccessModifier.Assignment4/src/accessmodifier/p2/Y.java
@@ -0,0 +1,14 @@
+package accessmodifier.p2;
+import accessmodifier.p1.M;
+import accessmodifier.p1.N;
+public class Y extends N {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new M().methodm();
+		new N().methodn();
+		new X().methodx();
+	}
+
+
+}
